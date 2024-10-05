31977bdb652cb215b7f436d6832f2d6984fc9e69
irisparadox <gasaav01@ucm.es>
2024-10-05 16:16:16 +0200

    Two Shader Programs

--- IRISEngine/src/main.cpp
+++ IRISEngine/src/main.cpp
@@ -8,11 +8,17 @@ const char* vertexShaderSource = "#version 460 core\n"
 								 "	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
 								 "}\0";
 
-const char* fragmentShaderSource = "#version 460 core\n"
-								   "out vec4 FragColor;\n"
-								   "void main() {\n"
-								   "	FragColor = vec4(0.85, 1.00, 0.62, 1.0);\n"
-								   "}\n";
+const char* tri_one_frag_source = "#version 460 core\n"
+								  "out vec4 FragColor;\n"
+								  "void main() {\n"
+								  "	FragColor = vec4(0.85, 1.00, 0.62, 1.0);\n"
+								  "}\n";
+
+const char* tri_two_frag_source = "#version 460 core\n"
+								  "out vec4 FragColor;\n"
+								  "void main() {\n"
+								  "	FragColor = vec4(0.26, 0.41, 1.00, 1.0);\n"
+								  "}\n";
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 	glViewport(0, 0, width, height);
@@ -65,59 +71,90 @@ int main() {
 		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << '\n';
 	}
 
-	unsigned int fragmentShader;
-	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	unsigned int tri_one_frag, tri_two_frag;
+	tri_one_frag = glCreateShader(GL_FRAGMENT_SHADER);
+	tri_two_frag = glCreateShader(GL_FRAGMENT_SHADER);
 
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
+	glShaderSource(tri_one_frag, 1, &tri_one_frag_source, NULL);
+	glCompileShader(tri_one_frag);
 
-	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	glShaderSource(tri_two_frag, 1, &tri_two_frag_source, NULL);
+	glCompileShader(tri_two_frag);
+
+	glGetShaderiv(tri_one_frag, GL_COMPILE_STATUS, &success);
 
 	if (!success) {
-		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		glGetShaderInfoLog(tri_one_frag, 512, NULL, infoLog);
 		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << '\n';
 	}
 
-	unsigned int shaderProgram;
-	shaderProgram = glCreateProgram();
-
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
-
-	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	glGetShaderiv(tri_two_frag, GL_COMPILE_STATUS, &success);
 
 	if (!success) {
-		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		glGetShaderInfoLog(tri_two_frag, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << '\n';
+	}
+
+	unsigned int tri_one_shader_prog, tri_two_shader_prog;
+	tri_one_shader_prog = glCreateProgram();
+	tri_two_shader_prog = glCreateProgram();
+
+	glAttachShader(tri_one_shader_prog, vertexShader);
+	glAttachShader(tri_one_shader_prog, tri_one_frag);
+	glLinkProgram(tri_one_shader_prog);
+
+	glAttachShader(tri_two_shader_prog, vertexShader);
+	glAttachShader(tri_two_shader_prog, tri_two_frag);
+	glLinkProgram(tri_two_shader_prog);
+
+	glGetProgramiv(tri_one_shader_prog, GL_LINK_STATUS, &success);
+
+	if (!success) {
+		glGetProgramInfoLog(tri_one_shader_prog, 512, NULL, infoLog);
 		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << '\n';
 	}
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
 
-	float vertices[] = {
-		-0.5f, -0.5f, 0.0f, // left  
-		 0.5f, -0.5f, 0.0f, // right 
-		 0.0f,  0.5f, 0.0f  // top   
+	glGetProgramiv(tri_two_shader_prog, GL_LINK_STATUS, &success);
+
+	if (!success) {
+		glGetProgramInfoLog(tri_two_shader_prog, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << '\n';
+	}
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(tri_one_frag);
+	glDeleteShader(tri_two_frag);
+
+	float tri_one_verts[] = {
+		-0.5f,  0.5f, 0.0f, // A
+		-1.0f, -0.5f, 0.0f, // B
+		-0.1f,  0.0f, 0.0f  // C
 	};
 
-	unsigned int VBO, VAO;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
+	float tri_two_verts[] = {
+		0.5f,  0.5f, 0.0f, // A
+		0.1f,  0.0f, 0.0f, // B
+		1.0f, -0.5f, 0.0f  // C
+	};
 
-	glBindVertexArray(VAO);
+	unsigned int vbo_arr[2], vao_arr[2];
+	glGenVertexArrays(2, vao_arr);
+	glGenBuffers(2, vbo_arr);
 
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	
+	glBindVertexArray(vao_arr[0]);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo_arr[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(tri_one_verts), tri_one_verts, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-	glBindVertexArray(0);
+	glBindVertexArray(vao_arr[1]);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo_arr[1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(tri_two_verts), tri_two_verts, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
 
 	// uncomment this call to draw in wireframe polygons.
-	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 	while (!glfwWindowShouldClose(window)) {
 		processInput(window);
@@ -125,13 +162,24 @@ int main() {
 		glClearColor(0.16f, 0.21f, 0.22f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO);
+		glUseProgram(tri_one_shader_prog);
+		
+		glBindVertexArray(vao_arr[0]);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glUseProgram(tri_two_shader_prog);
+
+		glBindVertexArray(vao_arr[1]);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+
+	glDeleteVertexArrays(2, vao_arr);
+	glDeleteBuffers(2, vbo_arr);
+	glDeleteProgram(tri_one_shader_prog);
+	glDeleteProgram(tri_two_shader_prog);
 
 	glfwTerminate();
 
